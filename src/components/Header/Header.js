0affc822aa5ffdd020b91e6d6549d997b6794c82
Kevin Real <kevinreal8994@gmail.com>
2022-02-07 20:59:46 +0100

    add image streaming to title bar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,28 @@
-import { Text } from "../../globalStyle";
 import styled from "styled-components";
 
-const AdriText = styled(Text)`
-  font-size: 40px;
+const HeaderBar = styled.div`
+  height: 50px;
+  width: 100%;
+  background: #fff;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  position: absolute;
+`;
+
+const StreamingImg = styled.img`
+  position: absolute;
+  width: 87px;
+  height: 16px;
+  left: 70px;
+  top: 3px;
+  src: ${"img/streaminglight.png"};
 `;
 
 const Header = () => {
   return (
     <>
-      <AdriText>buenos días</AdriText>
+      <HeaderBar>
+        <StreamingImg src={"img/streaminglight.png"}></StreamingImg>
+      </HeaderBar>
     </>
   );
 };
