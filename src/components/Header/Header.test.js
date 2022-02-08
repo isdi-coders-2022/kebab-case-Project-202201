47fd3fa389d570cca335e3596542374ace235cf4
Kevin Real <kevinreal8994@gmail.com>
2022-02-08 18:54:32 +0100

    change test description of Header.test

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -2,13 +2,13 @@ import { render, screen } from "@testing-library/react";
 import Header from "./Header";
 
 describe("Given Header component", () => {
-  describe("When it gets a Alt text /logo shawarma/", () => {
-    test("then it should found an image in Header with alt /logo shawarma/", () => {
+  describe("When it gets a image with src /logo shawarma/", () => {
+    test("then it should found an image with src /logo shawarma/", () => {
       render(<Header />);
 
       const streamingImg = screen.queryByAltText("logo shawarma");
 
-      expect(streamingImg).toHaveAttribute("alt", "logo shawarma");
+      expect(streamingImg).toBeInTheDocument();
     });
   });
 });
