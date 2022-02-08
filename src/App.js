36ccdf6b6a803835f4b1fe01d162b474654834ea
Josep Conejero <bermeu@gmail.com>
2022-02-08 12:15:33 +0100

    Installed prop-types and code in Streamer.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import Header from "./components/Header/Header";
+import Streamer from "./components/Streamer/Streamer";
+
+const streamerInfo = {
+  id: "124422593",
+  display_name: "LEC",
+  type: "online",
+  broadcaster_type: "partner",
+  description:
+    "Official Twitch Channel of the League of Legends European Championship. For more information, schedules and stats head to Lolesports.com",
+  profile_image_url:
+    "https://static-cdn.jtvnw.net/jtv_user_pictures/41b23352-5c99-4eb2-974b-1e9b8120425e-profile_image-300x300.png",
+};
 
 function App() {
   return (
     <div>
       <Header />
       <p>Hola</p>
+      <Streamer props={streamerInfo} />
     </div>
   );
 }
--- src/components/Streamer/Streamer.test.js
+++ src/components/Streamer/Streamer.test.js
@@ -4,7 +4,7 @@ import Streamer from "./Streamer";
 describe("Given the Streamer component", () => {
   describe("When it's rendered", () => {
     test("Then it should be in the document", () => {
-      const props = {
+      const streamerInfo = {
         id: "124422593",
         display_name: "LEC",
         type: "",
@@ -15,11 +15,11 @@ describe("Given the Streamer component", () => {
           "https://static-cdn.jtvnw.net/jtv_user_pictures/41b23352-5c99-4eb2-974b-1e9b8120425e-profile_image-300x300.png",
       };
 
-      render(<Streamer props={props} />);
+      render(<Streamer props={streamerInfo}></Streamer>);
 
-      const streamerElement = screen.queryByRole("Streamer");
+      const streamerElement = screen.getByText("partner");
 
-      expect(streamerElement).toBeTruthy();
+      expect(streamerElement).toBeInTheDocument();
     });
   });
 });
