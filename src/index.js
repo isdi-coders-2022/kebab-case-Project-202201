ad16f4db52df7afffd3fc8721a92d02aa63c550f
AdrianValladares <a.valladares.d@gmail.com>
2022-02-08 10:21:01 +0100

    added ThemeContextProvider to index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import ApiKebabContextProvider from "./components/contexts/ApiKebabContextProvider";
+import ThemeContextProvider from "./components/contexts/ThemeContextProvider";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ApiKebabContextProvider>
-      <App />
-    </ApiKebabContextProvider>
+    <ThemeContextProvider>
+      <ApiKebabContextProvider>
+        <App />
+      </ApiKebabContextProvider>
+    </ThemeContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
