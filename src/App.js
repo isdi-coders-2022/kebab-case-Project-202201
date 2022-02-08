88bc5d28bac7e98df073b34d2dbd49f0ad612261
Kevin Real <kevinreal8994@gmail.com>
2022-02-08 12:59:58 +0100

    Add navBar component and style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import Header from "./components/Header/Header";
+import NavBar from "./components/NavBar/NavBar";
+
 function App() {
   return (
-    <div>
-    </div>
+    <>
+      <Header />
+      <NavBar />
+    </>
   );
 }
 
