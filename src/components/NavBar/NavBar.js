0941f7c043ca392b40f1ccb20580878be3edd116
Kevin Real <kevinreal8994@gmail.com>
2022-02-08 16:11:07 +0100

    add navbar color from props.theme

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,10 +5,17 @@ const NavBarStyled = styled.div`
   bottom: 0;
   width: 100%;
   height: 65px;
-  background: #461094;
+  background: ${(props) => props.theme.accent};
   box-shadow: 0px -4px 4px rgba(0, 0, 0, 0.25);
   display: flex;
   justify-content: space-around;
+`;
+
+const MenuButton = styled.img`
+  width: 38px;
+  height: 47px;
+  padding-left: 15px;
+  align-self: flex-start;
 `;
 
 const NavBar = () => {
@@ -16,7 +23,9 @@ const NavBar = () => {
     <>
       <NavBarStyled>
         <button>hola</button>
-        <button>hola</button>
+        <button>
+          <MenuButton />
+        </button>
         <button>hola</button>
       </NavBarStyled>
     </>
