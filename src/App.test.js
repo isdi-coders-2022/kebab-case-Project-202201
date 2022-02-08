841cd47ebc6c514a61f99714fe251923417342bc
Kevin Real <kevinreal8994@gmail.com>
2022-02-08 17:44:56 +0100

    create snapshot test to app

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
+import App from "./App";
+import TestRenderer from "react-test-renderer";
+import ThemeContextProvider from "./store/contexts/ThemeContextProvider";
+import ApiKebabContextProvider from "./store/contexts/ApiKebabContextProvider";
+
 describe("desc", () => {
   describe("desc", () => {
-    test("desc", () => {
-      //Arrange
-      //Act
-      //Assert
-    });
+    test("desc", () => {});
+    const cosas = {
+      theme: "hola",
+      dispatch: "adiós",
+    };
+    const app = TestRenderer.create(
+      <ThemeContextProvider>
+        <ApiKebabContextProvider>
+          <App />
+        </ApiKebabContextProvider>
+      </ThemeContextProvider>
+    );
+    expect(app.toJSON()).toMatchSnapshot();
   });
 });
