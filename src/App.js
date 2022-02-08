1b79703b0c6cd1e7d5608012c39e13f918c40f84
AdrianValladares <a.valladares.d@gmail.com>
2022-02-08 12:32:37 +0100

    moved folders to conform with proposed structure

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { ThemeProvider } from "styled-components";
 import ThemeContext from "./components/contexts/ThemeContext";
 import FavoriteStreamer from "./components/FavoriteStreamer/FavoriteStreamer";
 import lightTheme from "./themes/lightTheme";
+import Header from "./components/Header/Header";
 
 function App() {
   const { initialValue } = useContext(ThemeContext);
   return (
     <ThemeProvider theme={lightTheme}>
       <div>
+        <Header />
         <FavoriteStreamer streamerInfo={{ display_name: initialValue }} />
       </div>
     </ThemeProvider>
