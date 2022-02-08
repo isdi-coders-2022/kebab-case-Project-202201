c7ff8a4e3d3cd5a37fccaefc126c9a3ef0ac8e9a
AdrianValladares <a.valladares.d@gmail.com>
2022-02-08 14:30:04 +0100

    Solved redundant imports in app.js

--- src/App.js
+++ src/App.js
@@ -5,21 +5,19 @@ import { ThemeProvider } from "styled-components";
 import ThemeContext from "./store/contexts/ThemeContext";
 import FavoriteStreamer from "./components/FavoriteStreamer/FavoriteStreamer";
 import lightTheme from "./themes/lightTheme";
-import Header from "./components/Header/Header";
 
 function App() {
   const { initialValue } = useContext(ThemeContext);
   return (
-
     <div>
-    <ThemeProvider theme={lightTheme}>
-      <div>
-    <Header />
-        <FavoriteStreamer streamerInfo={{ display_name: initialValue }} />
-<NavBar />
-      </div>
-    </ThemeProvider>
-</div>
+      <ThemeProvider theme={lightTheme}>
+        <div>
+          <Header />
+          <FavoriteStreamer streamerInfo={{ display_name: initialValue }} />
+          <NavBar />
+        </div>
+      </ThemeProvider>
+    </div>
   );
 }
 
