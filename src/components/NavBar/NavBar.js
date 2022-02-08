e38e41e6af56b2210da47fc37273fee725a9da16
Silvia Martinez <sil@silvia.com>
2022-02-08 17:48:33 +0100

    Add Component Button to nav-Bar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import ButtonText from "../ButtonText/ButtonText";
+import ButtonImage from "../ButtonImage/ButtonImage";
 
 const NavBarStyled = styled.div`
   position: fixed;
@@ -8,6 +10,7 @@ const NavBarStyled = styled.div`
   background: ${(props) => props.theme.accent};
   box-shadow: 0px -4px 4px rgba(0, 0, 0, 0.25);
   display: flex;
+  align-items: center;
   justify-content: space-around;
 `;
 
@@ -22,11 +25,12 @@ const NavBar = () => {
   return (
     <>
       <NavBarStyled>
-        <button>hola</button>
-        <button>
+        <ButtonText text={"HOME"} />
+        <ButtonImage />
+        {/* <button>
           <MenuButton src={"img/multidurum.png"} />
-        </button>
-        <button>hola</button>
+        </button> */}
+        <ButtonText text={"FAVS"} />
       </NavBarStyled>
     </>
   );
