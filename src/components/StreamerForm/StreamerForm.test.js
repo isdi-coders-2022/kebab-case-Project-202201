d4c5e786e931bc96fa0cdc342a6b68ed95c28942
Silvia Martinez <sil@silvia.com>
2022-02-09 12:39:27 +0100

    Added test in StreamerForm component

--- src/components/StreamerForm/StreamerForm.test.js
+++ src/components/StreamerForm/StreamerForm.test.js
@@ -3,12 +3,19 @@ import { render, screen } from "@testing-library/react";
 
 describe("Given a Form component", () => {
   describe("When it's rendered", () => {
-    test("Then it should render 'Image:", () => {
+    test("Then it should render 'Image:'", () => {
       render(<StreamerForm />);
 
       const renderedElement = screen.getByLabelText("Image:");
 
       expect(renderedElement).toBeInTheDocument();
+    });
+    test("Then it should render an input", () => {
+      render(<StreamerForm />);
+
+      const renderedInput = screen.getAllByRole("textbox");
+
+      expect(renderedInput).not.toBeNull();
     });
   });
 });
