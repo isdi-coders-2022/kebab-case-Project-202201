ae759a181775beb04b66fcda14527f0a10527b26
AdrianValladares <a.valladares.d@gmail.com>
2022-02-09 12:15:07 +0100

    added minor adjustments to test descriptions

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -5,7 +5,7 @@ import Header from "./Header";
 
 describe("Given Header component", () => {
   describe("When it gets a image with src /logo shawarma/", () => {
-    test("then it should found an image with src /logo shawarma/", () => {
+    test("then it should render an image with src /logo shawarma/", () => {
       render(<Header />);
 
       const streamingImg = screen.queryByAltText("logo shawarma");
