c4287288efb890fd79da635709b5f4196f9d181e
Silvia Martinez <sil@silvia.com>
2022-02-10 12:23:24 +0100

    Created Menu Sort

---
+++ src/components/MenuSearch/MenuSearch.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: rgba(98, 0, 238, 0.38);
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+`;
+const Menu = styled.h2`
+  background-color: ${(props) => props.theme.accentAlt2};
+  width: 350px;
+  height: 56px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  margin: 10px;
+`;
+
+const MenuSearch = (action) => {
+  return (
+    <>
+      <Container>
+        <Menu onClick={action}>SEARCH</Menu>
+      </Container>
+    </>
+  );
+};
+export default MenuSearch;
