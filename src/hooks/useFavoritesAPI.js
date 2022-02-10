ff736e08bed4005d7a5f231553baff73bed7bf14
AdrianValladares <a.valladares.d@gmail.com>
2022-02-10 11:58:11 +0100

    added sendstreamer function to custom hook

--- src/hooks/useFavoritesAPI.js
+++ src/hooks/useFavoritesAPI.js
@@ -1,1 +1,24 @@
-const useFavoritesAPI = () => {};
+const useFavoritesAPI = () => {
+  const favoritesEndpoint =
+    "https://shawarma-streaming.herokuapp.com/streamers";
+
+  const getAllFavorites = async () => {
+    const response = await fetch(favoritesEndpoint);
+    const data = await response.json();
+    return data;
+  };
+
+  const sendStreamer = async (streamer) => {
+    const packInfo = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: await JSON.stringify(streamer),
+    };
+    const response = await fetch(favoritesEndpoint, packInfo);
+    return await response.json();
+  };
+
+  return { getAllFavorites, sendStreamer };
+};
+
+export default useFavoritesAPI;
