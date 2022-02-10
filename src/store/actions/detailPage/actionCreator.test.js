8730d5b0618d018d67af4362a1edb38383b039e1
Kevin Real <kevinreal8994@gmail.com>
2022-02-10 13:21:31 +0100

    change description of actionCreator Test

--- src/store/actions/detailPage/actionCreator.test.js
+++ src/store/actions/detailPage/actionCreator.test.js
@@ -1,6 +1,6 @@
 import { loadEmotes, loadStreamer } from "./actionCreator";
 
-describe("Given a loadEmotes", () => {
+describe("Given a loadEmotes action", () => {
   describe("When is invoked", () => {
     test("Then it should return an action with /load-emotes/", () => {
       const expectedAction = { type: "load-emotes" };
@@ -10,7 +10,7 @@ describe("Given a loadEmotes", () => {
   });
 });
 
-describe("Given a loadStreamer", () => {
+describe("Given a loadStreamer action", () => {
   describe("When is invoked", () => {
     test("Then it should return an action with /load-info/", () => {
       const expectedAction = { type: "load-info" };
