8ab0f87a9c0f3d2d4288f61148ad43ad374c5f78
Silvia Martinez <sil@silvia.com>
2022-02-10 10:43:40 +0100

    Added another test to EmoteReducer.test.js

--- src/store/reducers/EmoteReducer.test.js
+++ src/store/reducers/EmoteReducer.test.js
@@ -13,4 +13,14 @@ describe("Given a EmotesReducer", () => {
       expect(recievedList).toEqual(newObjectList);
     });
   });
+  describe("When given a currentObjectList and a action that is not in actionCreator", () => {
+    test("Then should return the current list", () => {
+      const currentObjectList = [1, 2, 3, 4];
+      const action = { type: "not-work" };
+
+      const recievedList = EmotesReducer(currentObjectList, action);
+
+      expect(recievedList).toEqual(currentObjectList);
+    });
+  });
 });
