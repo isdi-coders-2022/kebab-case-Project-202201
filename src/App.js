752b57b7b9c99b3e8d53e4f1118a0c1748c96df3
Josep Conejero <bermeu@gmail.com>
2022-02-10 17:02:46 +0100

    Deleted more comments from App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { useContext } from "react";
 import { ThemeProvider } from "styled-components";
 import ThemeContext from "./store/contexts/ThemeContext";
 import { toggleThemeAction } from "./store/actions/theme/actionCreators";
-//import useTwitchAPI from "./hooks/useTwitchAPI";
 import { Navigate, Route, Routes } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import FormPage from "./pages/FormPage";
@@ -12,14 +11,9 @@ import FavoritesPage from "./pages/FavoritesPage";
 
 function App() {
   const { theme, dispatch } = useContext(ThemeContext);
-  // const { dispatchStreamerList } = useTwitchAPI();
   const toggleTheme = () => {
     dispatch(toggleThemeAction());
   };
-  /*useEffect(() => {
-    dispatchStreamerList();
-  }, [dispatchStreamerList]);
-  */
   return (
     <ThemeProvider theme={theme}>
       <Header actionOnClick={toggleTheme} />
@@ -30,8 +24,6 @@ function App() {
         <Route path="/Info/:id" element={<InfoPage />} />
         <Route path="/favorites" element={<FavoritesPage />} />
       </Routes>
-
-      {/* <FavoriteStreamer streamerInfo={streamerInfo} /> */}
     </ThemeProvider>
   );
 }
