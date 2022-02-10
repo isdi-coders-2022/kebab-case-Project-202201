da4cc4939c1b0040554f03f91e009196d837bd19
Kevin Real <kevinreal8994@gmail.com>
2022-02-10 14:26:50 +0100

    add test to check default statement of streamerinforeducer

--- src/store/reducers/StreamerInfoReducer.test.js
+++ src/store/reducers/StreamerInfoReducer.test.js
@@ -13,4 +13,14 @@ describe("Given a StreamerInfo reducer", () => {
       expect(recievedList).toEqual(newObjectList);
     });
   });
+  describe("When given a currentlist and a action that is not in actionCreators", () => {
+    test("Then should return the current list because its using default statement", () => {
+      const currentlist = ["ibai", "soryuga", "auronplay"];
+      const action = { type: "not-gonna-work" };
+
+      const reciebedList = StreamerInfoReducer(currentlist, action);
+
+      expect(reciebedList).toEqual(currentlist);
+    });
+  });
 });
