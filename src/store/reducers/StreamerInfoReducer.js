7cc28ac14138a138b4055b0f09763711509ace08
Kevin Real <kevinreal8994@gmail.com>
2022-02-10 14:10:20 +0100

    change actiontupe in streamerinforeducer to loadstreamerinfo

--- src/store/reducers/StreamerInfoReducer.js
+++ src/store/reducers/StreamerInfoReducer.js
@@ -3,7 +3,7 @@ import actionTypes from "../actions/detailPage/actionTypes";
 const StreamerInfoReducer = (currentStreamerInfo, action) => {
   let newStreamerInfo;
   switch (action.type) {
-    case actionTypes.loadEmotes:
+    case actionTypes.loadStreamerInfo:
       newStreamerInfo = [...action.loadStreamerInfo];
       break;
 
