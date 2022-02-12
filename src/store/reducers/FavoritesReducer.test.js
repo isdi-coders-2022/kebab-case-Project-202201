9b143d8d6c6e186c2804ecf813b030967958ab82
Josep Conejero <bermeu@gmail.com>
2022-02-12 20:26:09 +0100

    Modified FavoritesReducer.test.js

--- src/store/reducers/FavoritesReducer.test.js
+++ src/store/reducers/FavoritesReducer.test.js
@@ -4,8 +4,8 @@ import FavoritesReducer from "./FavoritesReducer";
 describe("Given a Favorites reducer", () => {
   describe("When given a current object and an action", () => {
     test("Then should return a newlist with currentObj and action", () => {
-      const currentlist = [1, 2, 3, 4];
-      const newList = [312, 543, 123, 54];
+      const currentlist = [10, 20, 30, 40];
+      const newList = [123, 436, 231, 45];
       const action = overwriteFavoritesAction(newList);
 
       const receivedList = FavoritesReducer(currentlist, action);
@@ -16,7 +16,7 @@ describe("Given a Favorites reducer", () => {
 
   describe("When given a currentlist and a action that is not in actionCreators", () => {
     test("Then should return the current list because its using default statement", () => {
-      const currentlist = [1, 2, 3, 4];
+      const currentlist = [23, 34, 47, 56];
       const action = { type: "not-gonna-work" };
 
       const receivedList = FavoritesReducer(currentlist, action);
