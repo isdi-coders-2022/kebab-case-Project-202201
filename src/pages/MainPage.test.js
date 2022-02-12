ec97728fd85f8eaac37c1b72af5f39f9329fa535
Josep Conejero <bermeu@gmail.com>
2022-02-12 18:45:46 +0100

    Added overWirteFavoritesAction (test)

--- src/pages/MainPage.test.js
+++ src/pages/MainPage.test.js
@@ -2,7 +2,7 @@ import MainPage from "./MainPage";
 import { render, screen } from "@testing-library/react";
 import MainContextProvider from "../store/contexts/MainPageContextProvider";
 
-describe("Given a Form page", () => {
+describe("Given a Main page", () => {
   describe("When it's rendered", () => {
     test("it should be render a label text", () => {
       render(
@@ -11,9 +11,9 @@ describe("Given a Form page", () => {
         </MainContextProvider>
       );
 
-      const text = screen.queryByLabelText("Name:");
+      const streamer = screen.queryByRole("container");
 
-      expect(text).toBeInTheDocument();
+      expect(streamer).toBeInTheDocument();
     });
   });
 });
